e2f5eeda3b2d6b3461a3d3ebe76defffe0effe34
AdiSharon <adi.sharon@campus.technion.ac.il>
2018-05-10 19:39:23 +0300

    2 error down!

--- Tree.h
+++ Tree.h
@@ -377,6 +377,7 @@ void Tree<T>::rotateLeft(Tree<T>::Node *root){
     root->father = newroot;
     updateHeight(root);
     updateHeight(newroot);
+    updateHeight(newroot->father);
 }
 
 template <class T>
